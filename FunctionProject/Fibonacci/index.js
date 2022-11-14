f401f9efb175ca0451a0d6ae0562af4a919f626f
ser0710 <serroz2822@gmail.com>
2022-11-14 17:42:07 -0500

    muestra de html

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,24 +1,43 @@
+var memo = new Array(1000001);
+var memori = function(n){
+    if(memo[n] != null){
+        return memo[n];
+    } else{
+        memo[n] = fibo(n)
+        return memo[n]
+    }
+}
+
+var fibo = function(n){
+    if(n<=1){
+        return 1
+    }else{
+        return memori(n-1) + memori(n-2)
+    }
+}
+
 import { one, zero } from "big-integer";
 export default async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    let nth = req.body.nth
+    
+    let n = req.body.nth
     let nth_1 = one;
     let nth_2 = zero;
     let answer = zero;
 
-    if (nth < 0)
+    if (n < 0)
         throw 'must be greater than 0'
-    else if (nth === 0)
+    else if (n === 0)
         answer = nth_2
     else if (nth === 1)
         answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+    else if(!memo[n]) {
+        //for (var i = 0; i < nth - 1; i++) {
+        //    answer = nth_2.add(nth_1)
+        //    nth_2 = nth_1
+        //    nth_1 = answer
+        //}
+        memori(nth);
     }
 
     context.res = {
